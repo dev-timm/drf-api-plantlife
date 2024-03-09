db594a932ace8389c462bdd62aabb909f1184959
dev-timm <timm.kuemmeth@gmail.com>
2024-03-09 10:52:28 +0000

    Create profiles app and model

--- drf_api_plantlife/settings.py
+++ drf_api_plantlife/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-devtimm-drfapiplantlife-xulro5ienqh.ws-eu108.gitpod.io']
 
 
 # Application definition
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
