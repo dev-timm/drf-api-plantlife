7be92f984679a74015f7f03bcc76e073e5459aa4
dev-timm <timm.kuemmeth@gmail.com>
2024-03-10 11:06:27 +0000

    post_image filter and size validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,21 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_post_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width is larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height is larger than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -15,5 +30,5 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'title', 'content', 'post_image', 'created_on', 'updated_on', 'is_owner', 'profile_id', 'profile_image' 
+            'id', 'owner', 'title', 'content', 'post_image', 'image_filter', 'created_on', 'updated_on', 'is_owner', 'profile_id', 'profile_image'
         ]
