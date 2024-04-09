6aaf062af93669a4bbeba60797ff97bbe2c3f616
dev-timm <timm.kuemmeth@gmail.com>
2024-04-09 11:02:27 +0000

    add bookmark list and detail view

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api_plantlife.permissions import IsOwnerOrReadOnly
+from .models import Bookmark
+from .serializers import BookmarkSerializer
 
-# Create your views here.
+
+class BookmarkList(generics.ListCreateAPIView):
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Bookmark.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class BookmarkDetail(generics.RetrieveDestroyAPIView):
+    serializer_class = BookmarkSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Bookmark.objects.all()
+
--- drf_api_plantlife/urls.py
+++ drf_api_plantlife/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
+    path('', include('bookmarks.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
 ]
