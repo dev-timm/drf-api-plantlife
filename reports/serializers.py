95469e0eedb2774996eb9b1ecb92f6c58b8d81ae
dev-timm <timm.kuemmeth@gmail.com>
2024-04-13 08:54:01 +0000

    add serializer for reports

--- reports/serializers.py
+++ reports/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Report
@@ -5,12 +6,11 @@ from .models import Report
 
 class ReportSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    created_on = serializers.SerializerMethodField()
 
-    class Meta:
-        model = Report
-        fields = [
-            'id', 'owner', 'post', 'report_reason', 'created_on'
-        ]
+
+    def get_created_on(self, obj):
+        return naturaltime(obj.created_on)
 
     def create(self, validated_data):
         try:
@@ -19,3 +19,9 @@ class ReportSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({
                 'detail': 'possible duplicate'
             })
+
+    class Meta:
+        model = Report
+        fields = [
+            'id', 'owner', 'post', 'report_reason', 'created_on'
+        ]
